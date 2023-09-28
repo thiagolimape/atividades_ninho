0d4e2d7c633c6cb7ec10d5daf4d25985924b3173
Thiago Lima <thiagolimads05@gmail.com>
2023-09-28 11:59:33 -0300

    atualizar

---
+++ 28.09 - Quinta/index.js
@@ -0,0 +1,13 @@
+document.getElementById('botao')
+.addEventListener('click', function(){
+    fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(function(response){
+        return response.json()
+    }).then(function(response){
+        console.log(response.sprites.front_default)
+
+        let url = response.sprites.front_default
+        document.getElementById('img-card').setAttribute('src', url)
+
+    })
+})
